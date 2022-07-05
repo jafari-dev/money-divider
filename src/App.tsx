3c5d5ead624b6d244536b7d5ddf331646e7e8c6d
Ahmad Jafari <me@jafari.dev>
2022-07-06 02:43:17 +0430

    Move the global contexts and HOCs to 'index.tsx' instead of 'App.tsx'

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,22 @@
-import { Global } from "@emotion/react";
 import { TopBar, NavigationMenu, GlobalWrapper, Home } from "@layouts";
-import { Container, ThemeProvider } from "@mui/material";
+import { Container } from "@mui/material";
 import { People } from "@pages";
-import { StoreProvider } from "@stores";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import { globalStyles } from "./styles";
-import { theme } from "./theme";
+import { Routes, Route } from "react-router-dom";
 
 function Application(): React.ReactElement {
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Global styles={globalStyles} />
-        <StoreProvider>
-          <TopBar />
-          <Container>
-            <GlobalWrapper>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/people/*" element={<People />} />
-              </Routes>
-            </GlobalWrapper>
-          </Container>
-          <NavigationMenu />
-        </StoreProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <>
+      <TopBar />
+      <Container>
+        <GlobalWrapper>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/people/*" element={<People />} />
+          </Routes>
+        </GlobalWrapper>
+      </Container>
+      <NavigationMenu />
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
+import { Global } from "@emotion/react";
+import { ThemeProvider } from "@mui/material";
+import { StoreProvider } from "@stores";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
 import Application from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { globalStyles } from "./styles";
+import { theme } from "./theme";
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
 
-root.render(<Application />);
+root.render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <Global styles={globalStyles} />
+      <StoreProvider>
+        <Application />
+      </StoreProvider>
+    </ThemeProvider>
+  </BrowserRouter>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
